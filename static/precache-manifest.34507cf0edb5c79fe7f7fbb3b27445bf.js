eed0408a4dba88e2de647cc5b8b4be347ed38a29
Sven Mol <s.mol@student.utwente.nl>
2019-09-27 13:46:55 +0200

    Add simple front-end

---
+++ static/precache-manifest.34507cf0edb5c79fe7f7fbb3b27445bf.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02af2bee75fd3b76ee843e65a8ab4376",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c951a3dcc31b39054132",
+    "url": "/static/js/2.50de4349.chunk.js"
+  },
+  {
+    "revision": "a327ec8a5a4e749a8494",
+    "url": "/static/js/main.60a9007c.chunk.js"
+  },
+  {
+    "revision": "6056c8735385c03bd8f1",
+    "url": "/static/js/runtime-main.ac27bce9.js"
+  }
+]);
---
+++ static/precache-manifest.ad5db45eac443875dd8fc76ff3d5d7ee.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5e19940a48c593ede24ab101e5c6a2c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3974f98d19d68d9688aa",
+    "url": "/static/js/2.e43b3107.chunk.js"
+  },
+  {
+    "revision": "44cc44adcc4de3457d42",
+    "url": "/static/js/main.920546c4.chunk.js"
+  },
+  {
+    "revision": "6056c8735385c03bd8f1",
+    "url": "/static/js/runtime-main.ac27bce9.js"
+  }
+]);
