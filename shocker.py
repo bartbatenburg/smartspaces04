b0c6e930f6711231257382269f2c73cebc6ac689
Sven Mol <s.mol@student.utwente.nl>
2019-09-27 11:08:58 +0200

    Remove debug statements

--- shocker.py
+++ shocker.py
@@ -4,22 +4,16 @@ from threading import Thread
 
 
 class Shocker:
-    channel = -1
-    led = None
-    state = False
-
     def __init__(self, channel):
         self.channel = channel
         self.led = LED(channel)
         self.led.on()
 
     def on(self):
-        print("Turning on channel %s" % self.channel)
         self.state = True
         self.led.off()
 
     def off(self):
-        print("Turning off channel %s" % self.channel)
         self.state = False
         self.led.on()
 
